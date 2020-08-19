c386a80f91bb4c6258a95106e739b0a1b1e8d65c
fadhlu <fadhlurahman.i@gmail.com>
2020-08-19 17:10:20 +0700

    remove trigger word

--- args.py
+++ args.py
@@ -21,8 +21,6 @@ def create_parser():
                         help="Connect to preferred db", default="menfess_twitter")
     parser.add_argument("-mf", "--menfess", type=str,
                         help="Choose menfess to connect", required=True)
-    parser.add_argument("-tr", "--trigger", type=str,
-                        help="Trigger word for menfess", required=True)
     parser.add_argument("-f", "--filter", help="Add file for filtered words")
     parser.add_argument(
         "-rf", "--refresh", help="Set refresh dm time interval", default=5, nargs='?', const=5, type=int)
